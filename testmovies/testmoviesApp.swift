aef21bafeec6606adec3adfc843ebe14c38ee940
Stanislav <stas.seryogin.87@gmail.com>
2023-09-20 11:41:54 +0300

    Initial Commit

---
+++ testmovies/testmoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  testmoviesApp.swift
+//  testmovies
+//
+//  Created by Stanislav Seryogin on 20.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct testmoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
